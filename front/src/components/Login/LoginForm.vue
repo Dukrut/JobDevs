2a4af8702942d1bf1b4e08e2c075119b8a1eac9a
Camille Neuburger <camillemneuburger@gmail.com>
2023-10-15 17:33:13 -0300

    (Frontend) - Adicionada validação dos campos para que não envie requisição caso seja vazio.

--- front/src/components/Login/LoginForm.vue
+++ front/src/components/Login/LoginForm.vue
@@ -2,14 +2,24 @@
   <div style="padding: 0px 20px; text-align: center;">
     <q-card class="my-card q-pa-lg">
       <h5>Entre na sua conta</h5>
-      <q-card-section>
-        <q-input class="q-mb-md" rounded outlined v-model="form.email" label="Usuário" />
-        <q-input rounded outlined v-model="form.password" label="Senha" />
-      </q-card-section>
 
-      <q-card-section>
-        <q-btn unelevated rounded color="primary" label="Entrar" @click="onSubimit" />
-      </q-card-section>
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-card-section>
+          <q-input class="q-mb-md" rounded outlined v-model="form.email" label="Usuário"
+            :rules="[val => !!val || 'Preencha o campo obrigatório!']" />
+
+          <q-input v-model="form.password" rounded outlined :type="isPwd ? 'password' : 'text'" label="Senha"
+            :rules="[val => !!val || 'Preencha o campo obrigatório!']">
+            <template v-slot:append>
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+            </template>
+          </q-input>
+        </q-card-section>
+
+        <q-card-section>
+          <q-btn unelevated rounded color="primary" label="Entrar" type="submit" />
+        </q-card-section>
+      </q-form>
 
       <q-card-section>
         <p>Ainda não possui uma conta? <a href="#">Cadastre-se aqui!</a></p>
@@ -27,16 +37,23 @@ const form = ref({
   password: null
 });
 
-const onSubimit = () => {
-  console.log(form.value);
-  login(form.value)
-    .then((response) => {
-      console.log("response", response);
-    })
-    .catch((error) => {
-      console.log("error", error);
-    })
+const isPwd = ref(true);
+
+const onSubmit = () => {
+  if (validateForm()) {
+    login(form.value)
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+  }
 }
+
+const validateForm = () => {
+  return form.value.email && form.value.password;
+};
 
 </script>
 
