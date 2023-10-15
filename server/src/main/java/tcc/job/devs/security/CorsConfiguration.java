7c87febb37ecbbeaa62fd992df7ae7b0a2f1af00
Eduardo Krutzsch <eduardo-krutzsch@hotmail.com>
2023-10-15 16:49:07 -0300

    #AJUSTES - AJustes na API, adicionado configuração de CORS. Adicionado usuários default.

--- server/src/main/java/tcc/job/devs/security/CorsConfiguration.java
+++ server/src/main/java/tcc/job/devs/security/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:9000")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
