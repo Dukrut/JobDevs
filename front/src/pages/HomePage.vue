9dd47755d408e2662c1882c7e1a2b83ccb65f819
Camille Neuburger <camillemneuburger@gmail.com>
2023-11-06 20:37:52 -0300

    Adicionando componentes na tela de home.

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,6 +1,54 @@
 <template>
   <div>
-    HomePage
+    <q-toolbar class="bg-primary text-white">
+      <q-toolbar-title>JobDevs</q-toolbar-title>
+
+      <q-btn flat round dense>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+      </q-btn>
+    </q-toolbar>
+  </div>
+
+  <div>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-btn push color="white" text-color="primary" label="Buscar por vagas" no-caps />
+    </div>
+
+    <q-separator inset />
+
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <q-card-section class="q-pt-xs">
+            <div class="text-overline">Overline</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+            <div class="text-caption text-grey">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore
+              magna aliqua.
+            </div>
+          </q-card-section>
+
+          <q-card-section class="col-5 flex flex-center">
+            <q-img class="rounded-borders" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+          </q-card-section>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions>
+          <q-btn flat round icon="event" />
+          <q-btn flat>
+            7:30PM
+          </q-btn>
+          <q-btn flat color="primary">
+            Reserve
+          </q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -9,3 +57,9 @@
 
 
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
