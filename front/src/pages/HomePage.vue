becafd881a38db25be5bcb3aed7630b78a74f3b8
Camille Neuburger <camillemneuburger@gmail.com>
2023-11-13 21:30:47 -0300

    Ajustes no layout e adicionado badges.

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -15,9 +15,10 @@
       <LoadingComponent />
     </div>
 
-    <div class="q-pa-md row items-start q-gutter-md" style="display: flex; justify-content: center; align-items: center;"
+    <div class="q-pa-lg row items-start q-gutter-md" style="display: flex; justify-content: center; align-items: center;"
       v-else>
-      <q-card class="my-card" flat bordered v-for="job, key in jobList" :key="key">
+      <q-card class="my-card" flat bordered v-for="job, key in jobList" :key="key"
+        style="display: -webkit-box; -webkit-line-clamp: 3; overflow: hidden; -webkit-box-orient: vertical; height: 500px;">
         <q-card-section horizontal>
           <q-card-section>
             <div class="text-overline row">
@@ -25,10 +26,28 @@
                 <img :src="job.careerPageLogo" width="25" alt="" style="vertical-align: middle;">
                 {{ job.careerPageName }}
               </div>
-              {{ job.similarity_score * 100 }}
-            </div>
+              <!-- {{ job.similarity_score * 100 }} -->
 
-            <!-- <div class="text-h5">{{ job.name }}</div> -->
+              <div class="col-1">
+                <q-badge v-if="job.similarity_score * 100 >= 66" rounded color="green">
+                  <q-tooltip>
+                    Essa vaga é super compatível com o seu perfil.
+                  </q-tooltip>
+                </q-badge>
+
+                <q-badge v-else-if="job.similarity_score * 100 >= 33" rounded color="yellow">
+                  <q-tooltip>
+                    Essa vaga pode ser possui alguns requisitos que correspondem ao seu perfil.
+                  </q-tooltip>
+                </q-badge>
+
+                <q-badge v-else rounded color="red">
+                  <q-tooltip>
+                    Essa vaga pode não ser muito interessando para o seu objetivo profissional.
+                  </q-tooltip>
+                </q-badge>
+              </div>
+            </div>
 
             <q-card-section style=" width: 320px; padding-left: 0px;">
               <h5 class="q-pt-none" style="margin: 0px;">{{ job.name }}</h5>
@@ -47,9 +66,9 @@
               <div v-if="job.country">Data de publicação: {{ job.publishedDate.substring(0,
                 job.publishedDate.indexOf('T'))
               }}</div>
-              <div v-if="job.country">País: {{ job.country }}</div>
-              <div v-if="job.state">Estado: {{ job.state }}</div>
-              <div v-if="job.city">Cidade: {{ job.city }}</div>
+              <div>País: {{ getValueOrDefault(job.country) }}</div>
+              <div>Estado: {{ getValueOrDefault(job.state) }}</div>
+              <div>Cidade: {{ getValueOrDefault(job.city) }}</div>
             </q-card-section>
 
             <q-card-section class="text-caption text-grey description q-pt-none"
@@ -61,7 +80,7 @@
 
         <q-separator />
 
-        <q-card-actions>
+        <q-card-actions style="margin-top: 17px;">
           <q-btn flat color="primary" :href="job.jobUrl" target="_blank" no-caps>
             Visualizar vaga
           </q-btn>
@@ -119,6 +138,13 @@ const getLabelJobTypeFormated = (code) => {
   }
 
   return jobType[code]
+}
+
+const getValueOrDefault = (text) => {
+  if (text.length)
+    return text;
+
+  return "Não disponível";
 }
 
 </script>
